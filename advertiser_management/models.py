95dc7047c8b0e56ca2551b09dd912a3850d2b842
alirezaei1380 <alirezaie1380@yahoo.com>
2021-01-25 22:48:25 +0330

    Add models Click and View.

--- advertiser_management/models.py
+++ advertiser_management/models.py
@@ -4,15 +4,11 @@ from django.db import models
 class Advertiser(models.Model):
     name = models.CharField(max_length=50, unique=True)
     id = models.AutoField(primary_key=True)
-    clicks = models.IntegerField(default=0, auto_created=True, editable=False)
-    views = models.IntegerField(default=0, auto_created=True, editable=False)
 
 
 class Ad(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=50)
-    clicks = models.IntegerField(default=0, auto_created=True, editable=False)
-    views = models.IntegerField(default=0, auto_created=True, editable=False)
     link = models.CharField(max_length=100)
     image = models.ImageField(default='', upload_to='images/')
     advertiser = models.ForeignKey(Advertiser, on_delete=models.CASCADE)
@@ -22,3 +18,22 @@ class Ad(models.Model):
         for ad in Ad.objects.all():
             if ad.id == ad_id:
                 return ad
+
+
+class CommonInfo(models.Model):
+    id = models.AutoField(primary_key=True)
+    ad_id = models.IntegerField()
+    time = models.DateTimeField()
+    ip = models.CharField()
+
+    class Meta:
+        abstract = True
+
+
+class View(CommonInfo):
+    pass
+
+
+class Click(CommonInfo):
+    pass
+
