0c28bfa63d8bda67398d8f5e08d71ea3cdc7c541
alirezaei1380 <alirezaie1380@yahoo.com>
2021-01-25 23:51:12 +0330

    Bug fixes.
    generating ip from request had bugs and deleted view and click increments from views.

--- advertiser_management/middleware.py
+++ advertiser_management/middleware.py
@@ -5,6 +5,15 @@ import datetime
 import re
 
 
+def get_ip(request):
+    ip_string = request.META.get('HTTP_X_FORWARDED_FOR')
+    if ip_string:
+        ip = ip_string.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+
 class ViewMiddleware:
 
     def __init__(self, get_response):
@@ -13,7 +22,7 @@ class ViewMiddleware:
     def __call__(self, request):
         if request.path.endswith("advertiser_management/"):
             for ad in Ad.objects.all():
-                view = View(ad_id=ad.id, time=datetime.datetime.now(), ip=request.META.get('HTTP_X_FORWARDED_FOR'))
+                view = View(ad_id=ad.id, time=datetime.datetime.now(), ip=get_ip(request))
                 view.save()
         response = self.get_response(request)
         return response
@@ -27,7 +36,7 @@ class ClickMiddleware:
     def __call__(self, request):
         match = re.match("/advertiser_management/(\d+)/", request.path)
         if match:
-            click = Click(ad_id=match.group(1), time=datetime.datetime.now(), ip=request.META.get('HTTP_X_FORWARDED_FOR'))
+            click = Click(ad_id=match.group(1), time=datetime.datetime.now(), ip=get_ip(request))
             click.save()
         response = self.get_response(request)
         return response
--- advertiser_management/views.py
+++ advertiser_management/views.py
@@ -7,8 +7,6 @@ from django.views.generic.edit import CreateView
 def index(request):
     context = {'advertisers': Advertiser.objects.iterator()}
     for ad in Ad.objects.all():
-        ad.views += 1
-        ad.advertiser.views += 1
         ad.save()
         ad.advertiser.save()
     return render(request, 'advertiser_management/ads.html', context=context)
@@ -16,8 +14,6 @@ def index(request):
 
 def ad(request, ad_id):
     ad = Ad.get_ad(ad_id)
-    ad.clicks += 1
-    ad.advertiser.clicks += 1
     ad.save()
     return redirect(ad.link)
 
