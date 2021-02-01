3c3f0f676085902d805ba7c407f837c0d40d013c
alirezaei1380 <alirezaie1380@yahoo.com>
2021-02-01 22:19:16 +0330

    Set rest framework setup.

--- YektaEduProjDjan/urls.py
+++ YektaEduProjDjan/urls.py
@@ -21,4 +21,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('advertiser_management/', include('advertiser_management.urls')),
     path('admin/', admin.site.urls),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('api-auth/', include('rest_framework.urls'))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
