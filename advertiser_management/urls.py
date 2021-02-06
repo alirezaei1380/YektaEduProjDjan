75e8e051fb08235a68b083b923882fcca46bf821
alirezaei1380 <alirezaie1380@yahoo.com>
2021-02-06 10:38:22 +0330

    Add features.
    new views and report page

--- advertiser_management/urls.py
+++ advertiser_management/urls.py
@@ -7,9 +7,15 @@ urlpatterns = [
     path('', views.AdListView.as_view(), name='AdListView'),
     path('<int:ad_id>/', views.AdRedirectView.as_view(), name='ad_link'),
     path('ad_form/', views.AdFormView.as_view(), name='AdFormView'),
-    path('report/', views.ReportView.as_view(), name='ReportView')
+    path('report/', views.ReportView.as_view(), name='ReportView'),
+    path('api/advertiser_management/report', views.AdView.as_view({
+        'get': 'report'
+    }))
 ]
 
 router = DefaultRouter()
-router.register(r'api/advertiser_management', views.AdView, basename='Advertiser_management')
+router.register(r'api/advertiser_management/ad', views.AdView, basename='Ad')
+router.register(r'api/advertiser_management/advertiser', views.AdvertiserView, basename='Advertiser')
+router.register(r'api/advertiser_management/click', views.ClickView, basename='Click')
+router.register(r'api/advertiser_management/view', views.ViewView, basename='View')
 urlpatterns += router.urls
--- advertiser_management/views.py
+++ advertiser_management/views.py
@@ -6,39 +6,36 @@ from django.views.generic.list import ListView
 from django.db.models import Count
 from django.db.models.functions import TruncHour
 from rest_framework.generics import CreateAPIView, ListAPIView, GenericAPIView
-from .serializers import AdSerializer, AdvertiserSerializer
+from .serializers import AdSerializer, AdvertiserSerializer, ClickSerializer, ViewSerializer
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
 from rest_framework.viewsets import ModelViewSet, ViewSet
 from rest_framework.response import Response
+import json
 
 
-class ReportView(TemplateView, GenericAPIView):
-    permission_classes = [IsAdminUser]
-    authentication_classes = [TokenAuthentication]
+def count(model):
+    return model.objects.count()
+
+def get_sum_by_id(model):
+    return model.objects.annotate(date=TruncHour('time')).values('date', 'ad_id')\
+        .annotate(count=Count('ad_id')).values('ad_id', 'date', 'count')
+
+def get_sum(model):
+     return model.objects.annotate(date=TruncHour('time')).values('date').\
+        annotate(count=Count('date')).values_list('date', 'count')
+
+
+class ReportView(TemplateView):
     template_name = 'advertiser_management/report.html'
-
-    @staticmethod
-    def count(model):
-        return model.objects.count()
-
-    @staticmethod
-    def get_sum_by_id(model):
-        return model.objects.annotate(date=TruncHour('time')).values('date', 'ad_id')\
-            .annotate(count=Count('ad_id')).values('ad_id', 'date', 'count')
-
-    @staticmethod
-    def get_sum(model):
-        return model.objects.annotate(date=TruncHour('time')).values('date').\
-            annotate(count=Count('date')).values_list('date', 'count')
 
     def get_context_data(self, **kwargs):
         context = super(ReportView, self).get_context_data(**kwargs)
-        context['view_sum'] = list(ReportView.get_sum_by_id(View))
-        context['click_sum'] = list(ReportView.get_sum_by_id(Click))
-        context['click_view_rate_sum'] = ReportView.count(Click) / ReportView.count(View)
-        views = list(ReportView.get_sum(View))
-        clicks = dict(ReportView.get_sum(Click))
+        context['view_sum'] = list(get_sum_by_id(View))
+        context['click_sum'] = list(get_sum_by_id(Click))
+        context['click_view_rate_sum'] = count(Click) / count(View)
+        views = list(get_sum(View))
+        clicks = dict(get_sum(Click))
         click_view_rate = {}
         for view in views:
             click_view_rate[view[0]] = clicks.get(view[0], 0) / view[1]
@@ -55,9 +52,7 @@ class ReportView(TemplateView, GenericAPIView):
         return context
 
 
-class AdListView(ListView, ListAPIView):
-    permission_classes = [AllowAny]
-    authentication_classes = [TokenAuthentication]
+class AdListView(ListView):
     model = Advertiser
     template_name = 'advertiser_management/ad_list.html'
 
@@ -74,9 +69,7 @@ class AdRedirectView(RedirectView):
         return ad.link
 
 
-class AdFormView(CreateView, CreateAPIView):
-    permission_classes = [IsAuthenticated]
-    authentication_classes = [TokenAuthentication]
+class AdFormView(CreateView):
     model = Ad
     template_name = 'advertiser_management/ad_form.html'
     fields = ['title', 'link', 'image', 'advertiser']
@@ -88,19 +81,6 @@ class AdView(ModelViewSet):
     serializer_class = AdSerializer
     queryset = Ad.objects.all()
 
-    def list(self, request):
-        serializer = AdSerializer(self.get_queryset(), many=True)
-        return Response(serializer.data)
-
-    def retrieve(self, request, pk=None):
-        ad = get_object_or_404(self.get_queryset(), pk=pk)
-        serializer = AdSerializer(ad)
-        return Response(serializer.data)
-
-    def create(self, request):
-        queryset = self.get_queryset()
-        queryset.create(request.data)
-
     def get_permissions(self):
         if self.action == 'retrieve':
             permission_classes = [IsAuthenticated]
@@ -108,4 +88,59 @@ class AdView(ModelViewSet):
             permission_classes = [IsAdminUser]
         else:
             permission_classes = [AllowAny]
+        return [permission() for permission in permission_classes]
+
+    def report(self, request):
+        data = {}
+        data['view_sum'] = list(get_sum_by_id(View))
+        data['click_sum'] = list(get_sum_by_id(Click))
+        data['click_view_rate_sum'] = count(Click) / count(View)
+        views = list(get_sum(View))
+        clicks = dict(get_sum(Click))
+        click_view_rate = {}
+        for view in views:
+            click_view_rate[view[0]] = clicks.get(view[0], 0) / view[1]
+        data['click_view_rate'] = {k: v for k, v in sorted(click_view_rate.items(),
+                                                              key=lambda item: item[1], reverse=True)}
+        time_difference = {}
+        clicks = Click.objects.all()
+        views = View.objects.all()
+        for click in clicks:
+            time_difference['ad_id:' + repr(click.ad_id)] = click.time - views.filter(ad_id=click.ad_id, ip=click.ip,
+                                                                                      time__lt=click.time) \
+                .order_by('-time').first().time
+        data['click_view_time_difference_average'] = time_difference
+        return Response(data)
+
+
+class AdvertiserView(ModelViewSet):
+
+    serializer_class = AdvertiserSerializer
+    queryset = Advertiser.objects.all()
+
+    def get_permissions(self):
+        if self.action == 'create':
+            permission_classes = [IsAdminUser]
+        else:
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
+
+
+class ClickView(ModelViewSet):
+
+    serializer_class = ClickSerializer
+    queryset = Advertiser.objects.all()
+
+    def get_permissions(self):
+        permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
+
+
+class ViewView(ModelViewSet):
+
+    serializer_class = ViewSerializer
+    queryset = Advertiser.objects.all()
+
+    def get_permissions(self):
+        permission_classes = [IsAdminUser]
         return [permission() for permission in permission_classes]
